ea56c42cd4ba2f1c089aa87908ab263b606f1a7f
isak.rabin <isak.rabin@rakuten.com>
2021-10-10 16:17:13 +0800

    [hacktoberfest-2021][leetcode]Implement strStr()

---
+++ leetcode/src/algorithms/P028/Solution.java
@@ -0,0 +1,11 @@
+package algorithms.P028;
+
+import java.util.*;
+
+public class Solution {
+
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+
+}
